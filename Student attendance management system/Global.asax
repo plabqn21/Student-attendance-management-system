7229dc463842ffaf1841572a31efb3d270457306
plabqn21 <plabqncsejnu@gmail.com>
2017-08-22 01:04:40 +0600

    InitialCommit

---
+++ Student attendance management system/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Student_attendance_management_system.MvcApplication" Language="C#" %>
---
+++ Student attendance management system/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Student_attendance_management_system.Startup))]
+namespace Student_attendance_management_system
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
