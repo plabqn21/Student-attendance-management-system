ad3636ece091ec87525e2494ed6cb56daad32c7a
plabqn21 <plabqncsejnu@gmail.com>
2017-08-28 13:24:51 +0600

    SecurityCAddedInRegister

--- Student attendance management system/Controllers/AccountController.cs
+++ Student attendance management system/Controllers/AccountController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Student_attendance_management_system.Models;
 using Student_attendance_management_system.Models.ViewModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Student_attendance_management_system.Controllers
 {
@@ -19,11 +16,15 @@ namespace Student_attendance_management_system.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+        private ApplicationDbContext db;
+
         public AccountController()
         {
+            db = new ApplicationDbContext();
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -35,9 +36,9 @@ namespace Student_attendance_management_system.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -121,7 +122,7 @@ namespace Student_attendance_management_system.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -150,12 +151,24 @@ namespace Student_attendance_management_system.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
+
+
+            var dbSecrectCode = db.SecrectCodes.SingleOrDefault(x => x.SecrectKey == model.SecrectCode);
+
+            if (dbSecrectCode == null)
+            {
+                @ViewBag.Message = "Invalid Secrect Key Please contact with Admin";
+                return View();
+
+            }
+
+
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser
                 {
                     UserName = model.Email,
-                   
+
                     Name = model.Name,
                     DateOfBirth = model.DateOfBirth,
                     Degree = model.Degree,
@@ -165,13 +178,13 @@ namespace Student_attendance_management_system.Controllers
                     ProfilePicture = model.ProfilePicture,
                     SecrectCode = model.SecrectCode
 
-                
+
                 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
